56e45143e3f1a7317d0f4f33c58914ebee5ffbc3
nguyen van bac <nguyenbac29305@gmail.com>
2025-08-26 09:27:22 +0700

    change domain

--- src/untils/request.js
+++ src/untils/request.js
@@ -1,4 +1,4 @@
-const API_DOMAIN = `http://localhost:3002/`;
+const API_DOMAIN = `https://onthisinhvien.vercel.app/database/db.json`;
 
 export const get = async (path) => {
     const response = await fetch(API_DOMAIN + path);
